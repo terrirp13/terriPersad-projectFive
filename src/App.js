27258fd83795e8c00e5084d178352cc25a76cfa3
terrirp <terri.persad@gmail.com>
2019-05-30 12:48:00 -0400

    first initialization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+ constructor(){
+   super();
+   this.state ={
+     items: [],
+     cityInput:'',
+     foodInput: '',
+   }
+ }
+
+ componentDidMount(){
+  
+ }
+ handleChange = (event) => {
+   this.setState({
+     userInput: event.target.value,
+   })
+ }
+
+
+  handleClick =(e) => {
+    e.preventDefault();
+    axios({
+      url: 'https://developers.zomato.com/api/v2.1/cities',
+      method: 'GET',
+      dataResponse: 'JSON',
+      headers: {
+        "accept": "application/json",
+        "x-requested-with": "xmlhttprequest",
+        "user-key": "0968faa2918c801deb6840af027c61de"
+      },
+      params: {
+        q: this.state.cityInput
+      }
+    }).then(function (result) {
+      console.log(result)
+    })
+  } 
+
+  render() {
+    return (
+      <div className="App">
+        <form action="submit">
+          <input onChange={this.handleChange} value={this.state.cityInput} name="cityInput" type="text"/>
+          <input onChange={this.handleChange} value={this.state.foodInput} name="foodInput" type="text" />
+          <button onClick={this.handleClick}>Submit</button>
+
+        </form>
+
+
+      </div>
+
+    );
+
+  }
+
 }
+
+
 
 export default App;
